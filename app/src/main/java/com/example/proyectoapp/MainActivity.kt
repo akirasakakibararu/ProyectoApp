57c8f8cc014620bba82ee7e05328c8082bc66144
Akira Sakakibara Ruiz <akira.Sakakibararu@elorrieta-errekamari.com>
2025-03-03 19:26:07 +0100

    Pequeñas modificaciones

--- app/src/main/java/com/example/proyectoapp/MainActivity.kt
+++ app/src/main/java/com/example/proyectoapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.proyectoapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +12,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val intent = Intent(this, PantallaPrincipalActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
