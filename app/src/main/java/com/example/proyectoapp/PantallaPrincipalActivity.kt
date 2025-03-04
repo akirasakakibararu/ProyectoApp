442cb770d7d710303f52fa215bdf3fd2aee2eca2
Akira Sakakibara Ruiz <akira.Sakakibararu@elorrieta-errekamari.com>
2025-03-04 12:57:52 +0100

    Añadidos algunos elementos a las pantallas, hay que modificar cosas

--- app/src/main/java/com/example/proyectoapp/PantallaPrincipalActivity.kt
+++ app/src/main/java/com/example/proyectoapp/PantallaPrincipalActivity.kt
@@ -1,6 +1,8 @@
 package com.example.proyectoapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +12,11 @@ class PantallaPrincipalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pantalla_principal)
+
+        val imageButton = findViewById<ImageButton>(R.id.imageButton)
+        imageButton.setOnClickListener{
+            val intent = Intent(this, PantallaProductosActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
