1af8cb0d413cfde328cde127af82d5bcf440b0c0
Akira Sakakibara Ruiz <akira.Sakakibararu@elorrieta-errekamari.com>
2025-03-07 19:12:19 +0100

    Activity añadir producto

--- app/src/main/java/com/example/proyectoapp/PantallaProductosActivity.kt
+++ app/src/main/java/com/example/proyectoapp/PantallaProductosActivity.kt
@@ -1,6 +1,8 @@
 package com.example.proyectoapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,13 @@ import androidx.core.view.WindowInsetsCompat
 class PantallaProductosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_pantalla_productos)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val imageButton = findViewById<ImageButton>(R.id.imageButton6)
+
+        imageButton.setOnClickListener{
+            val intent = Intent(this, AnnadirProductoActivity::class.java)
+            startActivity(intent)
         }
     }
 }
